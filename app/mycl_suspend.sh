e20bf176f9b166df4d67d24e2e2a657c5c0a348d
houhuihua <houhuihua@AppledeMacBook-Pro.local>
2019-01-04 12:02:42 +0800

    Added suspend_developer_rescan.py for mycl_suspend.sh

--- app/mycl_suspend.sh
+++ app/mycl_suspend.sh
@@ -1,6 +1,8 @@
 cd /home/min/dailyapp_googleplay/app
+cp /tmp/dailyapp.log ./
 date > /tmp/dailyapp_date_s.log
 pwd > /tmp/dailyapp_pwd_s.log
 export PYTHONIOENCODING=utf8
+/usr/bin/python2.7 suspend_developer_rescan.py >> /tmp/dailyapp.log 2>&1
 /usr/bin/python2.7 suspend_developer.py >> /tmp/dailyapp.log 2>&1
 
